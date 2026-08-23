5fa78786c5348804edcdedd4f77e9acbafd8269d
rmambulancier-pixel <rm.ambulancier@gmail.com>
2026-08-23 13:34:18 +0200

    Add lifecycle-viewmodel-compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.1")
     implementation("androidx.activity:activity-compose:1.9.0")
-    
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     // Jetpack Compose UI & Material 3
     implementation(platform("androidx.compose:compose-bom:2024.05.00"))
     implementation("androidx.compose.ui:ui")
